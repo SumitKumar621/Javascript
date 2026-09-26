82762614436a6ec85bb5e456bd44f58c61a39dcb
Sumit Kumar <2023ceb1050@iitrpr.ac.in>
2026-09-26 05:15:14 +0000

    dates

---
+++ 01basics/dates.js
@@ -0,0 +1,20 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let mycreateddate = new Date(2026,1,14,5,3)
+// console.log(mycreateddate.toLocaleString())
+
+let createdate = new Date("02-14-2023")
+console.log(createdate.toLocaleString())
+
+//-----Time-----
+let mytime = Date.now()
+
+// console.log(mytime);
+// console.log(createdate.getTime())
+
+ 
+
